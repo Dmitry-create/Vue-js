786d48888db2ab89a6ed46c893d0f9bc42a185d0
Dmitry <Shirdiman@mail.ru>
2021-07-01 21:42:35 +0300

    второй урок

--- vue2.x/src/components/calculator.vue
+++ vue2.x/src/components/calculator.vue
@@ -1,17 +1,44 @@
 <template>
     <div class="dis">
-        <input type=number v-model.number="operand1"/>
-        <input type=number v-model.number="operand2"/>=
+        <input type=number v-model.number="operand1" 
+        v-bind:disabled="check!==1">
+        <input type=number v-model.number="operand2"
+        v-bind:disabled="check!==2"/>=
         {{sum}}
         <div>
-            <button @click="sum=operand1+operand2">+</button>
-            <button @click="sum=operand1*operand2">*</button>
-            <button @click="sum=operand1-operand2">-</button>
-            <button @click="share(operand1,operand2)">/</button>
-            <button @click="shareHalf(operand1,operand2)">/.00</button>
-            <button @click="sum=Math.pow(operand1,operand2)">^n</button>
+            <button v-for="operand in operands" 
+            v-bind:key="operand"
+            v-bind:disabled="operand1 === '' || operand2 === ''"
+            @click="calculate(operand)">{{operand}}
+            </button>
+        </div>
+        <div>
+            <input type="checkbox" id="checkbox" v-model="checked">
+            <label>отобразить клавиатуру</label>
+        </div>
+        <div v-show="checked">
+            <button v-for="number in numbers"
+            v-bind:key="number"
+            @click="create(number)">
+            {{number}}
+            </button>
+            <button 
+            @click="del()">
+            del
+            </button>
+            
             
         </div>
+        <div class="radio">
+        <input name="radio" type="radio" 
+        @click="checking"
+        
+        >
+        <input name="radio" type="radio"
+        @click="checking2" >
+        </div>
+        
+        
     </div>
 </template>
 
@@ -20,35 +47,99 @@
         name: 'calculator',
         data(){
             return{
-                operand1: 0,
-                operand2: 0,
-                sum:0
+                operand1: '',
+                operand2: '',
+                sum:0,
+                operands: ['+', '-', '/', '*'],
+                checked:'',
+                numbers:[1,2,3,4,5,6,7,8,9,0,],
+                check: ''
             }
         },
         methods:{
-            share(oper1,oper2){
-                //а можно узнать почему так не работает?
+            // share(oper1,oper2){
+                
 
-                // if (oper2 = 0){
-                //    this.sum = "На ноль делить нельзя";
-                // }else{
-                //    this.sum = oper1/oper2;
-                // }
-                if (oper2 != 0){
-                   this.sum = oper1/oper2;
-                }else{
+            //     if (oper2 == 0){
+            //        this.sum = "На ноль делить нельзя";
+            //     }else{
+            //        this.sum = oper1/oper2;
+            //     }
+            //     // if (oper2 != 0){
+            //     //    this.sum = oper1/oper2;
+            //     // }else{
+            //     //    this.sum = "На ноль делить нельзя";
+            //     // }
+            // },
+            // shareHalf(oper1,oper2){
+            //     if (oper2 != 0){
+            //        this.sum = parseInt(oper1/oper2);
+            //     }else{
+            //        this.sum = "На ноль делить нельзя";
+            //     }
+            // }
+            add () {
+                this.sum = +this.operand1 +(+this.operand2)
+            },
+            substract () {
+                this.sum = this.operand1 - this.operand2
+            },
+            divide () {
+                if (this.operand2== 0){
                    this.sum = "На ноль делить нельзя";
+                }else{
+                   this.sum = this.operand1 / this.operand2
+                }  
+            },
+            multiply () {
+                this.sum = this.operand1 * this.operand2
+            },
+            calculate (operand = '') {
+                switch (operand) {
+                case '+':
+                    this.add()
+                    break;
+                case '-':
+                    this.substract()
+                    break;
+                case '*':
+                    this.multiply()
+                    break;
+                case '/':
+                    this.divide()
+                    break;
                 }
             },
-            shareHalf(oper1,oper2){
-                if (oper2 != 0){
-                   this.sum = parseInt(oper1/oper2);
-                }else{
-                   this.sum = "На ноль делить нельзя";
-                }
-            }
+            create(number){
+                if(this.check==1){
+                    this.operand1=this.operand1+String(number)
+                    }else{
+                     this.operand2=this.operand2+String(number)
+                    }
+                    console.log(this.check)
+                
+            },
+            checking(){
+               this.check=1
+               console.log(this.operand1)
+            },
+            checking2(){
+               this.check=2
+               console.log(this.operand2)
+            },
+            del(){
+                if(this.check==1){
+                     this.operand1=this.operand1.slice(0,-1)
+                    }else{
+                      this.operand2=this.operand2.slice(0,-1)
+                    }
+                    console.log(this.check)
+                
+               
+            }                            
         }
     }
+    
 </script>
 
 
